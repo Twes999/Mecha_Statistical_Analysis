930e32b1af6aff6f7f2ef75710c40ad73d750380
Taylor Wessel <t.j.wessel@Taylors-MacBook-Pro.local>
2022-03-08 01:35:49 -0500

    update for del 2.

--- R_Analysis/Challenge/MechaCarChallenge.RScript.R
+++ R_Analysis/Challenge/MechaCarChallenge.RScript.R
@@ -11,3 +11,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #6
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table))
+
+
+# Deliverable 2
+
+#2
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
+
+#3
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+#4
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
