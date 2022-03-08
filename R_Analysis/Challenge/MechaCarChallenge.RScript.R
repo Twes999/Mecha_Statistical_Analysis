f1b95bab1b75117315bffcec418899905af0ff22
Taylor Wessel <t.j.wessel@Taylors-MacBook-Pro.local>
2022-03-08 02:03:01 -0500

    update fro del 3.

--- R_Analysis/Challenge/MechaCarChallenge.RScript.R
+++ R_Analysis/Challenge/MechaCarChallenge.RScript.R
@@ -1,4 +1,5 @@
 # Deliverable 1
+library(tidyverse)
 
 #3
 library(dplyr)
@@ -23,3 +24,27 @@ total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Vari
 
 #4
 lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+
+
+# Deliverable 3
+
+#1
+sample_table <- coil_table %>% sample_n(35)
+plt <- ggplot(sample_table,aes(x=PSI))
+plt + geom_density()
+t.test(sample_table$PSI,mu=1500)
+
+
+#2
+sample_lot1_table <- coil_table %>% subset(Manufacturing_Lot=='Lot1') %>% sample_n(25)
+t.test(sample_lot1_table$PSI, mu=1500)
+
+sample_lot2_table <- coil_table %>% subset(Manufacturing_Lot=='Lot2') %>% sample_n(25)
+t.test(sample_lot2_table$PSI, mu=1500)
+
+
+sample_lot3_table <- coil_table %>% subset(Manufacturing_Lot=='Lot3') %>% sample_n(25)
+t.test(sample_lot3_table$PSI, mu=1500)
+
+
